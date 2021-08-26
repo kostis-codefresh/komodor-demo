a08b836e4bbca278517a5941c1a828131018e227
Kostis Kapelonis <kostis@codefresh.io>
2021-08-26 14:39:02 +0300

    added service dependency

--- loan/main.go
+++ loan/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
 )
@@ -25,7 +28,13 @@ func main() {
 		if err != nil {
 			loanAmount = 0
 		}
-		quote := offerQuote(loanAmount, 12)
+
+		quote := ""
+		interestFound, err := getInterestRate()
+		if err != nil {
+			quote = "Could not get interest. Sorry!"
+		}
+		quote = offerQuote(loanAmount, interestFound)
 
 		fmt.Fprintf(w, `<html>
 		<form method="post">
@@ -55,4 +64,29 @@ func offerQuote(loan int, interest int) string {
 	total := loan * interest / 100
 	return fmt.Sprintf("With rate %d%% you will pay  %d extra interest", interest, total)
 
+}
+
+func getInterestRate() (rate int, err error) {
+	url := "http://interest:8080/api/v1/in2terest"
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Printf("Could not access %s, got %s\n ", url, err)
+		return 0, errors.New("Could not access " + url)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		log.Println("Non-OK HTTP status:", resp.StatusCode)
+		return 0, errors.New("Could not access " + url)
+	}
+
+	log.Printf("Response status of %s: %s\n", url, resp.Status)
+
+	buf := new(bytes.Buffer)
+	_, err = buf.ReadFrom(resp.Body)
+	if err != nil {
+		return 0, err
+	}
+	log.Println("Found interest rate " + buf.String())
+	return strconv.Atoi(buf.String())
 }
